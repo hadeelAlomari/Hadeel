1730654ba7d333beaac2ba5ab8afaa3b2cb86231
hadeelAlomari <halomari0048@stu.kau.edu.sa>
2024-09-01 16:14:20 +0300

    commiting num 1

---
+++ src/hadeel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class hadeel {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        System.out.println("My name is Hadeel Alomari");
+        System.out.println("ID number: 2206020");
+        System.out.println("CPIT251 A2");
+    }
+}
